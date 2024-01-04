bd01812c64d9c13dc6c404b0edb9d99a572288dd
Herman100 <hermankwamebour30@gmail.com>
2024-01-04 23:21:28 +0000

    modify app notifications

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,10 +1,31 @@
 import React from "react";
 import "./Notifications.css";
+import { IoIosClose } from "react-icons/io";
+// import closeIcon from "./close-icon.png";
+import { getLatestNotification } from "./utils";
 
 function Notifications() {
     return (
-        <div className="Notifications ">
+        <div className="Notifications">
+            <button
+                style={{ position: "absolute", right: "8px", top: "6px", height: "20px",width: "30px" }}
+                aria-label="Close"
+                onClick={() => console.log("Close button has been clicked")}
+            >
+                <IoIosClose />
+                {/* Icons used instead of image */}
+                {/* <img src={closeIcon} alt="Close icon" /> */}
+            </button>
             <p>Here is the list of notifications</p>
+            <ul>
+                <li data-priority="default">New course available</li>
+                <li data-priority="urgent">New resume available</li>
+                <li
+                    dangerouslySetInnerHTML={{
+                        __html: getLatestNotification(),
+                    }}
+                ></li>
+            </ul>
         </div>
     );
 }
--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -9,3 +9,8 @@ export function getFooterCopy(isIndex) {
         ? "Holberton School"
         : "Holberton School main dashboard";
 }
+
+
+export function getLatestNotification() {
+    return '<strong>Urgent requirement</strong> - complete by EOD';
+}
